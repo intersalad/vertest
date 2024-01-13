80b1ac85276140af4e8dc8398da2352a4f565961
intersalad <140984613+intersalad@users.noreply.github.com>
2024-01-13 07:46:26 +0300

    .

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from supabase import create_client
 from gotrue.errors import AuthApiError
-from flask import Flask
+
 
 SUPABASE_URL="https://ocalnnljxiorkuhmxihp.supabase.co"
 SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jYWxubmxqeGlvcmt1aG14aWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQzMjcyMDksImV4cCI6MjAxOTkwMzIwOX0.HNoAukOyyqKRFx2AGBscRBe1uaqm2l2UkXQRMYM04GI"
@@ -10,7 +10,6 @@ SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZ
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
-@app.route
 
 
 email = "vladvolkov06@yandex.ru"
